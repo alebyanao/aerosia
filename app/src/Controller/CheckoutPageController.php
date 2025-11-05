88867f254678b3fbb70bcaa2bb601aab56ce6ec8
alebyanao <abelyaoctaviani07@gmail.com>
2025-11-05 16:02:20 +0700

    add all files

--- app/src/Controller/CheckoutPageController.php
+++ app/src/Controller/CheckoutPageController.php
@@ -245,7 +245,7 @@ class CheckoutPageController extends PageController
             }
 
             // HANDLE TIKET BERBAYAR - Lanjut ke payment gateway
-            return $this->redirect(Director::absoluteBaseURL() . 'payment/initiate/' . $order->ID);
+            return $this->redirect(Director::absoluteBaseURL() . '/payment/initiate/' . $order->ID);
 
         } catch (ValidationException $e) {
             $this->getRequest()->getSession()->set('CheckoutError', 'Validasi gagal: ' . $e->getMessage());
